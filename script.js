3d33be696d3ed3747edcfa65ca121719879848d0
Zachiah Zook <zakhazook1@gmail.com>
2020-07-24 17:18:52 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("demo").innerHTML = "Here are my experiments with Javascript:";
+var a; var b;
+a = "Hello"
+b = " Had popcorn for snack!"
+console.log(a+b); 
+document.write(Date());
+document.getElementById("image").src = "https://i.pinimg.com/originals/9e/41/6c/9e416c3aa91cf997ffc4a896b19cc1cc.jpg";
+
+     document.getElementById("p2").style.color = "white";
+     document.getElementById("p2").style.fontFamily = "Georgia";
+    document.getElementById("p2").style.fontSize = "larger";
+

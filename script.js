6f22fb48ca1a5caa54c356a41ef58cbc6ccf2818
Zachiah Zook <zakhazook1@gmail.com>
2020-08-04 18:02:50 +0000

    added labels

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 document.getElementById("demo").innerHTML = "Here are my experiments with Javascript:";
 var a; var b;
+
+//testing out console log
 a = "Hello"
 b = " Had popcorn for snack!"
-console.log(a+b); 
+console.log(a+b);
+
+//date 
 document.write(Date());
+
+//image
 document.getElementById("image").src = "https://i.pinimg.com/originals/9e/41/6c/9e416c3aa91cf997ffc4a896b19cc1cc.jpg";
 
+//JS style practice
      document.getElementById("p2").style.color = "white";
      document.getElementById("p2").style.fontFamily = "Georgia";
     document.getElementById("p2").style.fontSize = "larger";
